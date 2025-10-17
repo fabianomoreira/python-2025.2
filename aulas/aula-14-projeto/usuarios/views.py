4270a6c1d762402be5f83ea5817bf3d7d96f1a8f
Fabiano Moreira <fabiano.pinho@rj.senac.br>
2025-10-16 21:32:46 -0300

    Atualizações de aula

--- aulas/aula-14-projeto/usuarios/views.py
+++ aulas/aula-14-projeto/usuarios/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from visitantes.models import Visitante
+from porteiros.models import Porteiro
 
 def index(request):
     total_visitantes = Visitante.objects.all()
+    total_porteiros = Porteiro.objects.all()
   
     context = {
         'nome_pagina': 'Início da Dashboard',
-        'total_visitantes': total_visitantes
+        'total_visitantes': total_visitantes,
+        'total_porteiros': total_porteiros
     }
 
     return render(request, "index.html", context)
